4762ac8318f524594a333420bedfd9b03ecbdbe8
Nirmohan Singh <nirmohansingh19@gmail.com>
2020-11-18 12:25:22 -0500

    service requests call API - nirmohan

--- app_public/hm-services/src/app/dashboard/dashboard.component.ts
+++ app_public/hm-services/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerRequestService } from '../HMS services/customer-request.service';
+import { Request } from '../model/request';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  providers: [CustomerRequestService]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  serviceRequests: Request [];
 
-  ngOnInit(): void {
+  constructor(private customerRequestService: CustomerRequestService) { }
+
+  ngOnInit() {
+    this.customerRequestService.getCustomerServiceRequests().subscribe(result => {
+      this.serviceRequests = result;
+    }, error => {
+      console.log('error is ', error);
+    });
   }
 
 }
