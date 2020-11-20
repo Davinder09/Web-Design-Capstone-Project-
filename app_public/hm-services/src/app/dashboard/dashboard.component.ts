9ed2d8660b849f29dbefe2d3e311442f1d826f12
Davinder09 <56779475+Davinder09@users.noreply.github.com>
2020-11-20 01:56:07 -0500

    GET Employee on Service request

--- app_public/hm-services/src/app/dashboard/dashboard.component.ts
+++ app_public/hm-services/src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerRequestService } from '../HMS services/customer-request.service';
 import { Request } from '../model/request';
+import { Employee } from '../model/employee';
+import { EmployeeServiceService } from '../HMS services/employee-service.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  providers: [CustomerRequestService]
+  providers: [CustomerRequestService, EmployeeServiceService]
+
 })
 export class DashboardComponent implements OnInit {
 
   serviceRequests: Request [];
+  employees: Employee [];
 
-  constructor(private customerRequestService: CustomerRequestService) { }
+  constructor(private customerRequestService: CustomerRequestService, private employyeReq: EmployeeServiceService) { }
+
 
   ngOnInit() {
+    this.employyeReq.getEmployee().subscribe(result => {
+      this.employees = result;
+    }, error => {
+      console.log('error is ', error);
+    });
+
     this.customerRequestService.getCustomerServiceRequests().subscribe(result => {
       this.serviceRequests = result;
     }, error => {
